a26fb9751eb6f3ba25ef7ad401c2fd1bc45cf39c
Maxim <38497831+StableBig@users.noreply.github.com>
2023-08-07 01:51:13 +0200

    Output the winery age

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from datetime import datetime
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+def get_year_form(num):
+    if 11 <= num % 100 <= 20:
+        return "лет"
+    else:
+        if num % 10 == 1:
+            return "год"
+        elif 2 <= num % 10 <= 4:
+            return "года"
+        else:
+            return "лет"
+
+env = Environment(
+    loader=FileSystemLoader("."),
+    autoescape=select_autoescape(["html", "xml"])
+)
+
+template = env.get_template("template.html")
+
+current_year = datetime.now().year
+winery_age = current_year - 1920
+year_form = get_year_form(winery_age)
+
+rendered_page = template.render(
+    winery_age=f"{winery_age} {year_form}"
+)
+
+with open("index.html", "w", encoding="utf8") as file:
+    file.write(rendered_page)
+
+
+server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
