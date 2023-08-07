bc632fb327465dcd3193dba92044bf8e87cb86ed
Maxim <38497831+StableBig@users.noreply.github.com>
2023-08-07 03:02:30 +0200

    Add wine characteristics import from xlsx

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
-
+import pandas as pd
 
 def get_year_form(num):
     if 11 <= num % 100 <= 20:
@@ -21,12 +21,17 @@ env = Environment(
 
 template = env.get_template("template.html")
 
+excel_table_df = pd.read_excel("wine.xlsx")
+
+wine_characteristics = excel_table_df.to_dict(orient="records")
+
 current_year = datetime.now().year
 winery_age = current_year - 1920
 year_form = get_year_form(winery_age)
 
 rendered_page = template.render(
-    winery_age=f"{winery_age} {year_form}"
+    winery_age=f"{winery_age} {year_form}",
+    wine_characteristics=wine_characteristics
 )
 
 with open("index.html", "w", encoding="utf8") as file:
