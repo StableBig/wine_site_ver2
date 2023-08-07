67cd4a05fb1a7a4ca193d25ce8aa4347479bb317
Maxim <38497831+StableBig@users.noreply.github.com>
2023-08-07 03:59:52 +0200

    Categorize wines

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import pandas as pd
+from collections import defaultdict
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
-import pandas as pd
 
 def get_year_form(num):
     if 11 <= num % 100 <= 20:
@@ -21,17 +22,20 @@ env = Environment(
 
 template = env.get_template("template.html")
 
-excel_table_df = pd.read_excel("wine.xlsx")
-
-wine_characteristics = excel_table_df.to_dict(orient="records")
-
 current_year = datetime.now().year
 winery_age = current_year - 1920
 year_form = get_year_form(winery_age)
 
+wine_characteristics = pd.read_excel("wine2.xlsx")
+wine_characteristics = wine_characteristics.fillna("")
+
+grouped_characteristics = defaultdict(list)
+for _, row in wine_characteristics.iterrows():
+    grouped_characteristics[row["Категория"]].append(row.to_dict())
+
 rendered_page = template.render(
     winery_age=f"{winery_age} {year_form}",
-    wine_characteristics=wine_characteristics
+    grouped_characteristics=grouped_characteristics
 )
 
 with open("index.html", "w", encoding="utf8") as file:
